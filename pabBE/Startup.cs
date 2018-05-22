839696c1937cc6d79df94120e80903263dacd2c5
Mike Baugus <mbaugus@gmail.com>
2018-05-22 14:00:50 -0400

    hello

--- pabBE/Startup.cs
+++ pabBE/Startup.cs
@@ -27,7 +27,7 @@ namespace pabBE
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(Configuration.GetConnectionString("localmysql")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
